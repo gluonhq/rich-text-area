02357736401bd18a279e67895002b9fb9a8c9090
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2023-09-07 19:28:25 +0530

    remove unused import

--- samples/src/main/java/com/gluonhq/richtextarea/samples/BasicPromptDemo.java
+++ samples/src/main/java/com/gluonhq/richtextarea/samples/BasicPromptDemo.java
@@ -36,7 +36,6 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.List;
