ff0f20d77a633d53ef4ea8f703ad2aba249f7240
Johan Vos <johan@lodgon.com>
2025-11-28 09:34:45 +0100

    Do not require JavaFX 26 for regular building tests now use a tagged JavaFX 26 build make sure to use sw rendering in headless tests

--- rta/scripts/perftest.sh
+++ rta/scripts/perftest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CLASSPATH=$(mvn dependency:build-classpath -Dmdep.outputFile=/dev/stdout -q):target/test-classes:target/classes
+CLASSPATH=$(mvn -Ptest dependency:build-classpath -Dmdep.outputFile=/dev/stdout -q):target/test-classes:target/classes
 java -Dglass.platform=Headless -Dprism.order=sw -cp $CLASSPATH com.gluonhq.richtextarea.PerformanceTests
