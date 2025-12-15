789088926efb30afdb6571ee844c9e91d9101237
Johan Vos <johan@lodgon.com>
2025-12-15 09:41:36 +0100

    Process reviewer comments Override length, and override with exception remove

--- rta/src/main/java/com/gluonhq/richtextarea/model/AppendOnlyUnitBuffer.java
+++ rta/src/main/java/com/gluonhq/richtextarea/model/AppendOnlyUnitBuffer.java
@@ -55,6 +55,11 @@ public class AppendOnlyUnitBuffer extends UnitBuffer {
     }
 
     @Override
+    public void remove(int start, int end) {
+        throw new UnsupportedOperationException("Do not remove from an append-only buffer");
+    }
+
+    @Override
     public Unit getUnitWithRange(int start, int end) {
         if (start < 0 || unitList.isEmpty()) return new TextUnit("");
         int index = Arrays.binarySearch(unitLengths, 0, unitList.size(), start);
@@ -64,6 +69,11 @@ public class AppendOnlyUnitBuffer extends UnitBuffer {
             index = -(index) -1;
         }
         return unitList.get(index); 
+    }
+
+    @Override
+    public int length() {
+        return unitList.isEmpty() ? 0 : unitLengths[unitList.size() - 1];
     }
 
     private void ensureCapacity(int required) {
